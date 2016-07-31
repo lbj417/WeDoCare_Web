43f2dbe15485f0752c2a06f6b6ff8c2e1654667a
lbj417 <ljackson@mobiquityinc.com>
2016-07-31 19:09:02 -0400

    WDCWeb: Initial commit, file structure, and basic timeline view

---
+++ app/models/day.ts
@@ -0,0 +1,8 @@
+export class Day {
+  dayOfWeek: string;
+  formattedDate: string;
+  prescriptions: Array<Object>;
+  appointments: Array<Object>;
+  checklists: Array<Object>;
+  questionnaires: Array<Object>;
+}
